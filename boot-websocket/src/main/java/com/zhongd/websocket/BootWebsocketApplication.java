970dcf72641ecfc6e058c3d6e00a9fe2717bd77a
谢仲东 <zhongd@aliyun.com>
2019-01-08 22:17:33 +0800

    init

---
+++ boot-websocket/src/main/java/com/zhongd/websocket/BootWebsocketApplication.java
@@ -0,0 +1,14 @@
+package com.zhongd.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootWebsocketApplication.class, args);
+    }
+
+}
+
