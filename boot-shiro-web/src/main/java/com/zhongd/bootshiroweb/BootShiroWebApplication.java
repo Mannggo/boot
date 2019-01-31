cd7eedcb9234af178351a93130f4acbcd30e2aae
谢仲东 <zhongd@aliyun.com>
2019-02-01 00:26:46 +0800

    spring-boot 整合 shiro

---
+++ boot-shiro-web/src/main/java/com/zhongd/bootshiroweb/BootShiroWebApplication.java
@@ -0,0 +1,14 @@
+package com.zhongd.bootshiroweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootShiroWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootShiroWebApplication.class, args);
+    }
+
+}
+
---
+++ boot-shiro-web/src/test/java/com/zhongd/bootshiroweb/BootShiroWebApplicationTests.java
@@ -0,0 +1,17 @@
+package com.zhongd.bootshiroweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootShiroWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
